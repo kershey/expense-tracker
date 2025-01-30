3c8610f5b51823dbb52bbed5452786806e36eda2
Jensen Kershey Aragon <jnsnaragon01@gmail.com>
2025-01-31 01:04:40 +0800

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -17,7 +17,7 @@ export const Budgets = pgTable('budgets', {
 export const Expenses = pgTable('expenses', {
   id: serial('id').primaryKey(),
   name: varchar('name').notNull(),
-  amount: numeric('amount').notNull().default(0),
+  amount: numeric('amount').notNull().default('0'),
   budgetId: integer('budgetId').references(() => Budgets.id),
   createdAt: varchar('createdAt').notNull(),
 });
